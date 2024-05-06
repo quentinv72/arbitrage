466a13e72fa870c7634317ad31c7e421595fdf69
Quentin <quentin.vilchez@gmail.com>
2024-05-06 16:04:48 -0400

    create common blacklist

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,5 +1,14 @@
+use std::collections::HashSet;
+
 use ethers::middleware::Middleware;
 use ethers::signers::Signer;
+use ethers::types::Address;
+use once_cell::sync::Lazy;
 
 pub mod logging;
 pub mod utils;
+
+pub static TOKEN_BLACKLIST: Lazy<HashSet<Address>> = Lazy::new(|| {
+    let mut set = HashSet::new();
+    set
+});
