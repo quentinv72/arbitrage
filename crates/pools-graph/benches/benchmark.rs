ae316442aeecf5ce2b565c39d69f9c76dafabae3
Quentin <quentin.vilchez@gmail.com>
2024-06-10 11:07:02 +0200

    add some benchmarking

--- crates/pools-graph/benches/benchmark.rs
+++ crates/pools-graph/benches/benchmark.rs
@@ -1,8 +1,14 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::sync::Arc;
+
+use criterion::{black_box, Criterion, criterion_group, criterion_main};
+use ethers::prelude::{Http, Provider};
 use ethers::types::{Address, U256, U64};
+use ethers::utils::Anvil;
+use revm::db::{CacheDB, EthersDB};
 
 use pools_graph::pool_data::pool_data::PoolDataTrait;
 use pools_graph::pool_data::uniswap_v2::UniswapV2;
+use pools_graph::pool_data::uniswap_v3::UniswapV3;
 use utils::placeholder_middleware::PlaceholderMiddleware;
 
 pub fn uniswap_v2_amount_out(c: &mut Criterion) {
@@ -26,5 +32,44 @@ pub fn uniswap_v2_amount_out(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, uniswap_v2_amount_out);
+pub fn uniswap_v3_amount_out(c: &mut Criterion) {
+    let anvil = Anvil::new()
+        .fork("https://eth-sepolia.g.alchemy.com/v2/fEmCuDGqB-tSA4R5HnnVCy1n9Jg4GqJg@6077409")
+        .spawn();
+
+    let provider = Arc::new(Provider::<Http>::try_from(anvil.endpoint()).unwrap());
+
+    let pool = UniswapV3::new(
+        "0x9799b5EDC1aA7D3FAd350309B08df3F64914E244"
+            .parse()
+            .unwrap(),
+        U256::zero(),
+        "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8"
+            .parse()
+            .unwrap(),
+        "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"
+            .parse()
+            .unwrap(),
+        3,
+        None,
+    );
+
+    let ethers_db = EthersDB::new(provider, None).unwrap();
+
+    let mut cache_db = CacheDB::new(ethers_db);
+
+    UniswapV3::load_quoter_bytecode(&mut cache_db);
+
+    c.bench_function("v3_amount_out", |b| {
+        b.iter(|| {
+            pool.get_amount_out(
+                black_box(U256::from(1000)),
+                black_box(true),
+                Some(&mut cache_db),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, uniswap_v2_amount_out, uniswap_v3_amount_out);
 criterion_main!(benches);
