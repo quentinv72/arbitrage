337e2fada913de01c9f4fc4425a06b441ebc8373
Quentin V <35984401+quentinv72@users.noreply.github.com>
2024-06-18 12:24:31 +0200

    Create generic arbitrage tx object (#16)
    * Create generic executor tx
    
    * more updates

--- crates/pools-graph/src/arbitrage/mod.rs
+++ crates/pools-graph/src/arbitrage/mod.rs
@@ -1,2 +1,21 @@
-pub mod arb_tx;
+use ethers::addressbook::Address;
+use ethers::prelude::{Bytes, U256};
+use ethers::providers::Middleware;
+
+use crate::arbitrage::arbs::ArbPool;
+use crate::pools_graph::PoolsGraph;
+
+pub mod arb_tx_v1;
 pub mod arbs;
+
+
+pub(crate) trait ArbTx {
+    fn new(targets: Vec<ArbPool>, amounts_in: Vec<U256>, amounts_out: Vec<U256>) -> Self;
+    fn build_tx(
+        &self,
+        pools_graph: &PoolsGraph,
+        executor_address: Address,
+        output_token: Address,
+    ) -> Bytes;
+    fn estimated_profit(&self) -> U256;
+}
